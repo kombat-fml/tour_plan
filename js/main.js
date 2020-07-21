a1871489ae54538f88837fc384bc669794eadff8
Anton Shakhov <informativpeople@gmail.com>
2020-07-21 20:00:32 +0300

    create: added yandex map with using api

--- js/main.js
+++ js/main.js
@@ -12,3 +12,40 @@ var mySwiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 });
+
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map('map', {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      // Чтобы не определять координаты центра карты вручную,
+      // воспользуйтесь инструментом Определение координат.
+      center: [7.99, 98.291],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 16,
+    }),
+    myGeoObject = new ymaps.GeoObject(
+      {
+        // Описание геометрии.
+        geometry: {
+          type: 'Point',
+          coordinates: [7.99, 98.291],
+        },
+        // Свойства.
+        properties: {
+          // Контент метки.
+          iconContent: 'Hotel here',
+        },
+      },
+      {
+        // Опции.
+        // Иконка метки будет растягиваться под размер ее содержимого.
+        preset: 'islands#blackStretchyIcon',
+        // Метку можно перемещать.
+        draggable: false,
+      }
+    );
+  myMap.geoObjects.add(myGeoObject);
+}
